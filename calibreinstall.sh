46f7fe4c1d805247bef0ff4e9a25ce8c8298d75d
aptalca <thedjinn@yahoo.com>
2015-04-20 12:07:16 -0400

    Delete calibreinstall.sh

--- calibreinstall.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-  cd /root/calibre && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
-  
