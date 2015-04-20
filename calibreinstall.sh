d3ae1e147b950e44d26ea0365975192d1dc608f6
aptalca <thedjinn@yahoo.com>
2015-04-19 23:16:50 -0400

    Update calibreinstall.sh

--- calibreinstall.sh
+++ calibreinstall.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-  sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+  wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
   
